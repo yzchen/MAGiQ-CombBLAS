790032480c84f0bedfcc415bc0e5d7a685867c26
cheny0l <yuanzhao.chen@kaust.edu.sa>
2018-04-28 18:32:33 +0300

    lubm100k dataset queries && all queries for two datasets

--- run/100k-nodes1024-cores4096.sh
+++ run/100k-nodes1024-cores4096.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --account=k1285
 #SBATCH --job-name=lubm10240_l7
-#SBATCH --output=/project/k1285/CombBLAS/run/lubm10240_l7_nodes16.log
+#SBATCH --output=/project/k1285/CombBLAS/run/lubm100k_nodes1024_cores4096.log
 #SBATCH --error=/project/k1285/CombBLAS/run/err.log
 #SBATCH --time=00:30:00
 #SBATCH --threads-per-core=1
-#SBATCH --nodes=16
-#SBATCH --ntasks-per-node=16
+#SBATCH --nodes=1024
+#SBATCH --ntasks-per-node=4
 #SBATCH --exclusive
 
-srun /project/k1285/CombBLAS/build/selfTests/lubm10240_l7 0
+srun /project/k1285/CombBLAS/build/selfTests/lubm100k
