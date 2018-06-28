e82ef14f83266bfe7412e938871e37cdc7185c4e
cheny0l <yuanzhao.chen@kaust.edu.sa>
2018-06-28 15:34:35 +0300

    get local dcsc and local indices (I, J, V)

--- selfTests/GetDCSCTest.cpp
+++ selfTests/GetDCSCTest.cpp
@@ -7,14 +7,32 @@
 using namespace std;
 using namespace combblas;
 
-template<class NT>
+#define IndexType int
+#define ElementType int
+
 class PSpMat {
 public:
-    typedef SpDCCols<int, NT> DCCols;
-    typedef SpParMat<int, NT, DCCols> MPI_DCCols;
+    typedef SpDCCols<IndexType, ElementType> DCCols;
+    typedef SpParMat<IndexType, ElementType, DCCols> MPI_DCCols;
 };
 
-#define ElementType int
+// fill I and J, they should have same size
+void get_indices_local(PSpMat::MPI_DCCols &M, vector<IndexType> &I, vector<IndexType> &J, vector<ElementType> &V) {
+    auto d0 = M.seq().GetInternal();
+    I.assign(d0->ir, d0->ir + d0->nz);
+    V.assign(d0->numx, d0->numx+d0->nz);
+
+    for (int index = 0; index < d0->nzc; ++index) {
+        int times = d0->cp[index + 1] - d0->cp[index];
+
+        for (int i = 0; i < times; ++i) {
+            J.push_back(d0->jc[index]);
+        }
+    }
+
+    // if does not have same size, wrong
+    assert(I.size() == J.size());
+}
 
 int main(int argc, char *argv[]) {
     int nprocs, myrank;
@@ -33,29 +51,50 @@ int main(int argc, char *argv[]) {
 
     {
         shared_ptr<CommGrid> fullWorld;
-        fullWorld.reset( new CommGrid(MPI_COMM_WORLD, 0, 0) );
+        fullWorld.reset(new CommGrid(MPI_COMM_WORLD, 0, 0));
 
         string Mname(argv[1]);
 
         MPI_Barrier(MPI_COMM_WORLD);
 
-        PSpMat<ElementType>::MPI_DCCols A(MPI_COMM_WORLD);
+        PSpMat::MPI_DCCols A(MPI_COMM_WORLD);
 
         double t1 = MPI_Wtime();
         A.ReadDistribute(Mname, 0);
 //        A.ParallelReadMM(Mname, true, maximum<ElementType>());
         double t2 = MPI_Wtime();
-        if(myrank == 0) {
+        if (myrank == 0) {
             cout << "read file takes " << t2 - t1 << " s" << endl;
         }
         A.PrintInfo();
 
-//        auto d0 = A.seq().GetInternal(0);
+        // get I and J
+//        auto d0 = A.seq().GetInternal();
+////
+////        cout << "myrank : " << myrank << "\t";
+////        cout << "nz : " << d0->nz << "\t";
+////        cout << "nzc : " << d0->nzc << endl;
+////
+        vector<IndexType> I, J, V;
+//        for (int index = 0; index < d0->nzc; ++index) {
+//            int times = d0->cp[index + 1] - d0->cp[index];
 //
-//        if (myrank == 0) {
-//            cout << "nnz : " << d0->nz << endl;
-//            cout << "nzc : " << d0->nzc << endl;
+//            for (int i = 0; i < times; ++i) {
+//                J.push_back(d0->jc[index]);
+//            }
 //        }
+//
+//        cout << myrank << "\t" << I.size() << "\t" << J.size() << endl;
+
+        get_indices_local(A, I, J, V);
+
+//        cout << myrank << "\t" << I.size() << "\t" << J.size() << endl;
+        cout << "myrank : " << myrank << "\t";
+        for (int i = 0; i < I.size(); ++i) {
+            cout << I[i]+1 << "," << J[i]+1 << "," << V[i] << "\t";
+        }
+        cout << endl;
+
     }
 
     MPI_Finalize();
